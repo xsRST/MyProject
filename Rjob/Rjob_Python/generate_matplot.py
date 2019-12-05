69493f60157a3b771de361ac6dd9fb44237bb5b5
shuai.xia <1871117608@qq.com>
2019-12-05 15:52:03 +0800

    Rjob :Update

--- Rjob/Rjob_Python/generate_matplot.py
+++ Rjob/Rjob_Python/generate_matplot.py
@@ -4,9 +4,10 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import jobs.ConfigChina
+from conf import ConfigChina
 
 matplotlib.style.use('ggplot')
+
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 mpl.rc('xtick', labelsize=10)  # 设置坐标轴刻度显示大小
@@ -16,7 +17,7 @@ plt.rcParams['figure.dpi'] = 300
 
 out_path = "C:\JINNA\\trunk/"
 filname = out_path + "GenusHistIntervalStats20191127.csv"
-data = pd.read_csv(filname, names=jobs.ConfigChina.GenusHistIntervalStats_header, encoding="utf-8", engine='c')
+data = pd.read_csv(filname, names=ConfigChina.GenusHistIntervalStats_header, encoding="utf-8", engine='c')
 new_data = data[data["Symbol"] == "00005.HK"]
 new_data.reset_index(drop=True, inplace=True)
 ax1 = new_data.plot(color="blue", x="StartTime", y=["Volume"], kind="bar", sharex=True, grid=True)
